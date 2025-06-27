110e671ef3cf56dc6961b7d89a0c31c9b9632ebe
Yogeshwar B. <byogeshwarreddy@gmail.com>
2025-06-27 16:14:58 +0530

    Clean up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
-import webpage from './assets/browser.svg'
-import dev from '/dev.svg'
-import styles from './App.module.css'
-
 function App() {
   return (
     <>
-      <div className={styles['flex-row']}>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={dev} className={styles['img-logo']} alt='Dev Icon' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img
-            src={webpage}
-            className={styles['img-logo']}
-            alt='Webpage Icon'
-          />
-        </a>
-      </div>
       <h1>Yogeshwar.dev</h1>
     </>
   )
