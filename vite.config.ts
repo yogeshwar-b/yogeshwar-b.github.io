3c9bfff2c447b0e41d3c7e2c2b0f641a06006bea
Yogeshwar B. <byogeshwarreddy@gmail.com>
2025-06-27 19:20:42 +0530

    Added TailWind CSS and Shadcn UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()]
+plugins: [react(), tailwindcss()],
+resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
 })
