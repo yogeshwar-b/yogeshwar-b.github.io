ce33da03d91ac8d0596b6e02a5dfc823f4736116
Yogeshwar B. <40350972+yogeshwar-b@users.noreply.github.com>
2025-07-03 00:31:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -378,7 +378,7 @@ function App() {
           <h2 className='text-2xl font-semibold mb-4'>Awards & Achievements</h2>
           <ul className='list-disc pl-6 space-y-2 text-lg'>
             <li>
-              <b>Secured a Rank of 1288 in GATE 2017</b> – Top 1% of around 1
+              <b>Secured a Rank of 1288 in GATE CS 2017</b> – Top 1% of around 1
               Lakh applicants.
             </li>
             <li>
